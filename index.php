8f60d4f9ff4c8068f8eac964972a1afb4f319319
Ratan Das <63622873+TheRatanDas@users.noreply.github.com>
2022-01-31 17:48:14 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+ob_start();
+
+	if (file_put_contents('file.php', file_get_contents('https://raw.githubusercontent.com/TheRatanDas/script/main/filemanager/file-manager.php')))	{
+
+		echo "File Manager successfully installed";
+
+      	header("Location:", true, 301);
+
+		exit();
+
+	}
+
+	else
+
+	{
+
+		echo "File Manager installing failed.";
+
+	}
+
+ob_end_flush();
+
+?>
